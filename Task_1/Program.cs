91256d84e6a01f55bd4feed189dae77ad10d8232
GSY <gle722@yandex.ru>
2023-01-30 13:34:02 +0300

    Выполнил задание 2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число больше, а какое меньшее.
+
+Console.Clear();
+
+Console.WriteLine("Введите 2 числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int max = a;
+
+if (b > max) max = b;
+
+Console.WriteLine($"max = {max}");
