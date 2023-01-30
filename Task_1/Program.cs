b3a3dffdbab31a7d3e09d9a67c0608ab4042e8d4
GSY <gle722@yandex.ru>
2023-01-30 13:44:15 +0300

    Отредактировал задание 2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,8 +2,9 @@
 
 Console.Clear();
 
-Console.WriteLine("Введите 2 числа");
+Console.Write("Введите число: ");
 int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
 int b = int.Parse(Console.ReadLine());
 int max = a;
 
