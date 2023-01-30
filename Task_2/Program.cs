e15bb3b740f0cef081ddd72e80cdcf46acd0821b
GSY <gle722@yandex.ru>
2023-01-30 13:43:09 +0300

    Выполнил задание 4

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine($"max = {max}");
