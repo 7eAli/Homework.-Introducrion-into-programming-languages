39cca3ad8b8278b38e432fb5f1208db4e1e639ed
GSY <gle722@yandex.ru>
2023-01-30 15:09:52 +0300

    Выполнил задание 13

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a < 100 && a > -100) Console.WriteLine("Третьей цифры нет");
+else
+{
+    while (a > 999 || a < -999)
+    {
+        a = a / 10;
+    }
+    a = a % 10;
+    if (a < 0) a = -a;
+    Console.WriteLine($"Третья цифра {a}");
+}
