93e0347fc42836bce695d48892abd575d844cb7d
GSY <gle722@yandex.ru>
2023-01-30 14:02:48 +0300

    Выполнил задание 8

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int output = 0;
+
+while (output < num - 1)
+{
+    output = output + 2;
+    Console.Write($"{output} ");
+}
+
