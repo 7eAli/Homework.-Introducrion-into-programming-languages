ab825c42422b571d3617242dc3957499b2a0f374
GSY <gle722@yandex.ru>
2023-02-09 10:14:59 +0300

    Выполнил ДЗ 2 семинара 4

---
+++ Task_12/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Написать программу вычисления произведения чисел от 1 до N */
+
+int factorial(int num)
+{
+    int prod = 1;     
+    if (num == 0) return prod;                                                  
+    else                                                                            
+    {                                                                                
+        for (int i = 1; i <= num; i++)
+        {
+            prod = prod * i;
+        }
+        return prod;
+    }
+}
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()!);
+if (num < 0) Console.WriteLine("Факториала отрицательного числа не существует");    // Вариант, если в условии имелся ввиду факториал.
+else                                                                                // Если в условии имелось ввиду последовательное произведение,
+{                                                                                   // то пишем Console.WriteLine(0), т.к. от 1 до отрицательного N
+    int prod = factorial(num);                                                      // всегда будет проход через 0, обнуляющий все произведение
+    Console.WriteLine($"Факториал числа {num} равен {prod}");
+}
