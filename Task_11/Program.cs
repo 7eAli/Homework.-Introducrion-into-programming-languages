a95b31f6b23b3aa6aedb30d006cca0f1518d35f0
GSY <gle722@yandex.ru>
2023-02-09 09:52:03 +0300

    Выполнил ДЗ 1 семинара 4

---
+++ Task_11/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Подсчитать сумму цифр в числе */
+
+int digSumCounter(int num)
+{
+    int sum = 0;    
+    if (num < 0) num = -num;    
+    while(num != 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int sum = digSumCounter(num);
+Console.Write($"Сумма цифр числа {num} равна {sum}");
+
+
