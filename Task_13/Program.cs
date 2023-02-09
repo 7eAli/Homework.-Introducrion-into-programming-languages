c6b985ea6c014b71ebca807eaf5fb0c430a2fcd7
GSY <gle722@yandex.ru>
2023-02-09 11:42:57 +0300

    Выполнил ДЗ 3 семинара 4

---
+++ Task_13/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Показать кубы чисел, заканчивающихся на четную цифру */
+
+// Решение с вводом диапазона чисел
+void evenNumToPow3(int num1, int num2)
+{
+    if (num1 < num2)
+    {
+        for (int i = num1; i <= num2; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write($"{i * i * i} ");       
+                if (num2 % 2 == 1 && i != num2 - 1) Console.Write(", ");
+                else if(num2 % 2 == 0 && i != num2) Console.Write(", ");
+
+            }
+        }
+    }
+    else
+    {
+        for (int i = num2; i <= num1; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write($"{i * i * i} ");
+                if (num1 % 2 == 1 && i != num1 - 1) Console.Write(", ");
+                else if(num1 % 2 == 0 && i != num1) Console.Write(", ");         
+            }
+        }
+    }
+}
+
+Console.Clear();
+
+Console.Write("Введите 1 границу диапазона: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 границу диапазона: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+evenNumToPow3(num1, num2);
