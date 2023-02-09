fe433f0902e73d528e45e499572109a857538e04
GSY <gle722@yandex.ru>
2023-02-09 09:39:12 +0300

    Выполнил задание 23

---
+++ Task_10/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N */
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()!);
+int counter = 1;
+
+if (num > 0)
+{
+    while(num >= counter)
+    {
+        Console.Write($"{counter * counter * counter}");
+        if (num != counter) Console.Write(", ");
+        counter += 1;
+    }
+}
+else
+{
+    while(num <= counter)
+    {
+        Console.Write($"{counter * counter * counter}");
+        if (num != counter) Console.Write(", ");
+        counter -= 1;
+    }
+}
