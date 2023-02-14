90e54cfab64e25c44c33ad32992b482f497e4ec0
GSY <gle722@yandex.ru>
2023-02-14 17:14:21 +0300

    Решил задание 36

---
+++ Task_15/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами
+// Найдите сумму элементов, стоящих на нечётных позициях
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int SumOddPosElem(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1) sum += array[i];
+    }
+    return sum;
+}
+
+Console.Clear();
+
+int[] array = GetArray(10, -10, 10);
+Console.WriteLine(string.Join(", ", array));
+
+Console.WriteLine($"Сумма элементов на нечетых позициях равна {SumOddPosElem(array)}");
