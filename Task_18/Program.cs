9f380a60c88ae51c18069c77536733dcee4e7645
GSY <gle722@yandex.ru>
2023-02-14 18:20:38 +0300

    Решил задание 41

---
+++ Task_18/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] CreateArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i + 1}-е число массива: ");
+        result[i] = Convert.ToInt32(Console.ReadLine()!);
+    }
+    return result;
+}
+
+int CountPositive(int[] array)
+{
+    int count = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j] > 0) count++;
+    }
+    return count;
+}
+
+
+Console.Clear();
+int[] array = CreateArray(5);
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine($"Положительных чисел: {CountPositive(array)}");
