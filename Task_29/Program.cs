d480826601ca0424045a91fe1b2fdc8d05feaf8d
GSY <gle722@yandex.ru>
2023-02-28 09:39:31 +0300

    Решил задание 64

---
+++ Task_29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void ShowNumbers(int N)
+{
+    if (N == 1)
+    {
+        Console.Write(1);
+        return;
+    }
+    else
+    {
+        Console.Write($"{N}, ");
+        ShowNumbers(N - 1);
+        return;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+ShowNumbers(N);
